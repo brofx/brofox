b3372de98791e8078405bb6024e8772ceb45ec69
Scott Adie <scottynosabe@gmail.com>
2016-02-02 19:02:37 -0500

    Changes

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
-
+from random import choice
 
 app = Flask(__name__)
 
+
+backgrounds = [
+"ark.jpg",
+"rl.jpg",
+"csgo.jpg",
+"dota2.jpg"
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", bg=choice(backgrounds))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001, host="0.0.0.0")
